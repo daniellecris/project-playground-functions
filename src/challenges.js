521acb8a6b87577ae4a9e90ca0d8d6469b4fe246
Danielle Cristina <dnaielle.trybe@gmail.com>
2021-11-17 14:44:08 -0300

    Desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -33,11 +33,11 @@ function concatName(stringArray) {
 }
 
 // Desafio 5
-// function footballPoints(wins, ties) {
-//   // seu código aqui
-//   let resultado = ((3 * wins) + ties);
-//   return resultado;
-// }
+function footballPoints(wins, ties) {
+  // seu código aqui
+  let resultado = ((3 * wins) + ties);
+  return resultado;
+}
 
 // Desafio 6
 // function highestCount(numRepete) {
